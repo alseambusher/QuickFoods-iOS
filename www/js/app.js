3d18345fdf0c03b479a1d1963b556f22067c8a92
Suresh Alse <alseambusher@gmail.com>
2014-10-17 12:03:40 +0530

    schema change, items page base

--- www/js/app.js
+++ www/js/app.js
@@ -37,9 +37,18 @@
 
   module.controller('TakeOrderController', function($scope, $data) {
     // todo: remove this and add actual code
+    $scope.items = $data.ordered_items; 
+    $scope.showDetail = function(index) {
+      var selectedItem = $data.ordered_items[index];
+      $data.selectedItem = selectedItem;
+      $scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
+    };
+  });
+
+
+  module.controller('ListItemsController', function($scope, $data) {
+    // todo: remove this and add actual code
     $scope.items = $data.items; 
-    $scope.categories = $data.categories;  
-    $scope.default_categories = $data.categories[0].title;
     $scope.showDetail = function(index) {
       var selectedItem = $data.items[index];
       $data.selectedItem = selectedItem;
@@ -52,7 +61,7 @@
   module.factory('$data', function() {
       var data = {};
       
-      data.items = [
+      data.ordered_items = [
           { 
               title: 'Veg Manchow Soup',
               category: 'Veg',
@@ -79,18 +88,24 @@
           }
       ]; 
 
-      data.categories = [
-
-          {
-              title: 'Veg'
+      data.items = [
+          { 
+              title: 'Veg Manchow Soup',
+              category: 'Veg',
           },
-          {
-              title: 'Non Veg'
+          { 
+              title: 'Gobi Manchurian',
+              category: 'Veg',
           },
-          {
-              title: 'Drinks'
+          { 
+              title: 'Chilly Chicken',
+              category: 'Non Veg',
           },
-      ];
+          { 
+              title: 'Pepsi',
+              category: 'Drinks',
+          }
+      ]; 
       
       return data;
   });
